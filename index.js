d47a121f6e632039dce406241e22cafb95e9c9ef
Fabián Tureo <fabianestebantureo@gmail.com>
2021-08-03 12:36:33 -0300

    First Commit - Configure server

--- index.js
+++ index.js
@@ -12,35 +12,4 @@ app.listen(PORT, ()=> {
 app.use(express.json());
 
 //  ----- Endpoint #1 ----- //
-//Define date
-const todayIs = new Date().toLocaleDateString()
-const timeIs = new Date().toLocaleTimeString()
-
-app.get('/api/today', (req, res) => {
-    res.send(`You have requested the date at this time: ${todayIs}, ${timeIs}`)
-})
-
-//  ----- Endpoint #2 ----- //
-//Require module fs
-const fs = require('fs')
-
-//Call the method fs of read file. Use readFile for asynchronous process
-app.get('/api/reading', (req, res) => {
-    fs.readFile('text.txt', 'utf-8', (err, data)=> {
-        //If error exists
-        (err) && res.send(`This is the error: ${err}`)
-        //Response
-        res.send(`The response is: ${data}`)
-        console.log(data)
-    })
-})
-
-//  ----- Endpoint #3 ----- //
-// If don't use app.use(express.json()), will not work
-// Testing with Postman sending JSON in body
-
-app.post('/api/greeting', (req,res) => {
-    (req.body === undefined) ?
-    res.send('This is undefined or an error has been ocurred') :
-    res.send(`Buenos dias ${req.body.name}!`)
-})
+// Login with authorization
